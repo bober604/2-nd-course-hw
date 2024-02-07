074f06c35887bb7bcf50528b14994d20b3568423
bober_604 <ivanarh604@gmail.com>
2024-02-07 22:34:42 +0500

    teste

---
+++ style/hw-6.js
@@ -0,0 +1,11 @@
+// const numbs = [2, 4, 5, 6, 8];
+
+// numbs.forEach((el) => {
+// 	console.log(el);
+// });
+
+// const numbs = [2, 4, 5, 6, 8];
+
+// numbs.forEach((el, index) => {
+// 	console.log(`${index}: ${el}`);
+// });
